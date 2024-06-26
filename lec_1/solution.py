850d02c25553008a1b23d31fd296ea3ece0da32f
hiral-mehta <hiral.j.mehta06@gmail.com>
2024-06-26 22:58:16 +0530

    Update solution.py

--- lec_1/solution.py
+++ lec_1/solution.py
@@ -1,1 +1,71 @@
 #write your answer here
+#(Q.1)take an input from user is should be a number and find out how many digit it has
+
+# nums = [6, 4, 14, 15]
+# target = 18
+# a=len(nums)
+
+# sum = {}
+
+# for i in range(a):
+#     num1 = target - nums[i]
+#     if nums[i] in sum:
+#         result = [sum[nums[i]], i]
+#         break
+#     sum[num1] = i
+
+# print(result)
+
+
+
+#(Q.2)take an input from the user the factorial of a number
+#if the user enters a string print not a valid input
+
+# import math
+
+# value = input("Enter a value: ")
+
+# if value.isdigit():
+#     num = int(value)
+#     if num >= 0:
+#         fact = 1
+#         for i in range(1, num + 1):
+#             fact = fact * i
+#         print("The factorial of",num,"is",fact)
+#     else:
+#         print("Factorial does not exist for negative numbers.")
+# else:
+#     print("Not a valid input.")
+
+
+
+#(Q.3)
+
+# Computers ={
+#   "Laptop1" : {"brand" : "DELL","OS" : "Windows"},
+#   "Laptop2" : {"brand" : "HP", "OS" : "Linux"},
+#   "Desktop" : {"brand" : "Apple","OS" : "Mac-OS"}
+# }
+# brand=[Computers["Laptop1"]["brand"],Computers["Laptop2"]["brand"],Computers["Desktop"]["brand"]]
+# OS=[Computers["Laptop1"]["OS"],Computers["Laptop2"]["OS"],Computers["Desktop"]["OS"]]
+# print(brand)
+# print(OS)
+
+
+
+#leetcode question
+
+# nums = [6, 4, 14, 15]
+# target = 18
+# a=len(nums)
+
+# sum = {}
+
+# for i in range(a):
+#     num1 = target - nums[i]
+#     if nums[i] in sum:
+#         result = [sum[nums[i]], i]
+#         break
+#     sum[num1] = i
+
+# print(result)
